ecd77cc2a843d5f23a323a191f6b41f0fcedbced
Cristina <cristina.perez@scmspain.com>
2015-08-18 09:36:44 +0200

    component generation with generator sui

---
+++ src/index.jsx
@@ -0,0 +1,3 @@
+import Carousel from './sui-carousel';
+
+export default {Carousel};
---
+++ src/sui-carousel/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class Carousel extends React.Component {
+  render() {
+    return (
+      <div className='sui-Carousel'>Create your component here</div>
+    );
+  }
+}
