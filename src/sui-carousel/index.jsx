f079ef9369c8e8c9ba07de6fba9c5f587f697b7c
Cristina <cristina.perez@scmspain.com>
2015-08-21 12:07:42 +0200

    adding demo photos

--- src/sui-carousel/index.jsx
+++ src/sui-carousel/index.jsx
@@ -16,7 +16,7 @@ export default class Carousel extends React.Component {
     return (
       <div className='sui-Carousel'>
         <ReactSwipe continuous={true}>
-            <div><img height='500px' alt='Sello de montaña y ola japonesa carvados a mano #stamping #crafts" src="https://igcdn-photos-d-a.akamaihd.net/hphotos-ak-xfa1/t51.2885-15/11326163_148497685488515_9386340_n.jpg' /></div>
+            <div><img height='500px' alt='Sello de montaña y ola japonesa carvados a mano #stamping #crafts' src='https://igcdn-photos-d-a.akamaihd.net/hphotos-ak-xfa1/t51.2885-15/11326163_148497685488515_9386340_n.jpg' /></div>
             <div><img height='500px' alt='acuareleando' src='https://igcdn-photos-g-a.akamaihd.net/hphotos-ak-xaf1/t51.2885-15/11809729_711469768984806_1727706550_n.jpg' /></div>
             <div><img height='500px' alt='curry rojo tailandes' src='https://igcdn-photos-g-a.akamaihd.net/hphotos-ak-xaf1/t51.2885-15/11355147_915182411888366_323491906_n.jpg' /></div>
         </ReactSwipe>
