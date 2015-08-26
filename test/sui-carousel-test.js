9ba30580d50a32e0ff8e4bce1c9dff79354faef4
Cristina <cristina.perez@scmspain.com>
2015-08-26 13:29:40 +0200

    test update

--- test/sui-carousel-test.js
+++ test/sui-carousel-test.js
@@ -3,12 +3,14 @@ import TestUtils from 'react/lib/ReactTestUtils';
 import {createComponent} from './utilities';
 import expect from 'expect';
 import Carousel from '../src/sui-carousel';
+import Slide from '../src/sui-slide';
 
 describe('sui-carousel component test suite', function () {
 
   describe('loading', function() {
     it('component is loaded properly', function () {
       expect(Carousel).toNotBe(undefined);
+      expect(Slide).toNotBe(undefined);
     });
   });
 
@@ -16,7 +18,7 @@ describe('sui-carousel component test suite', function () {
     let component;
 
     beforeEach(() => {
-      component = createComponent(Carousel);
+      component = createComponent(Carousel, {data:[]});
     });
 
     afterEach(() => {
